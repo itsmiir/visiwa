109110308551ee54019a6375a75c8e4df30dae16
itsmiir <itsmiir@users.noreply.github.com>
2022-05-14 17:39:37 -0500

    python generator prototype

---
+++ src/main/java/com/miir/visiwa/Visiwa.java
@@ -0,0 +1,11 @@
+package com.miir.visiwa;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Visiwa implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("test");
+
+    }
+}
